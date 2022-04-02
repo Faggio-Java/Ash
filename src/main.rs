d0ba07b5e1d11cf920412fc7e557c734c21dc0a0
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-04-02 13:35:26 -0600

    Rewrote The Hostname Fetcher

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use sysinfo::{System, SystemExt};
 use std::process::Command;
 use std::ffi::OsString;
+use std::fs::File;
 
 fn main() {
-let sys = System::new_all();
+let hostname = File::open("/etc/hostname").unwrap();
+
+let info = Command::new("cat")
+        .stdin(hostname)
+        .output()
+        .expect("Error doing whatever"); 
 
 let kernel = Command::new("uname")
         .arg("-r")
@@ -26,11 +31,10 @@ let uptime = Command::new("uptime")
         .expect("Error doing whatever");
 
 let total_memory = format!("{}", String::from_utf8_lossy(&memory.stdout).replace("MemTotal:      ", ""));
-let title = format!("{}@{}", os_string("USER"), sys.host_name().unwrap());
 
 print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
-println!("        {}\n ===========================", title);
+println!("       {}@{} ===========================",  os_string("USER"), String::from_utf8_lossy(&info.stdout));
 println!("OS: {}Kernel: {}Uptime: {}Environment: {} \nShell: {} \nMemory: {} ===========================",
 String::from_utf8_lossy(&os.stdout).replace("NAME=", ""), 
 String::from_utf8_lossy(&kernel.stdout), 

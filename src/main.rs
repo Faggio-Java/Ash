f80620d68b81f821b66d7c493a65370964d34ff1
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-04-02 20:43:53 -0600

    Rewrote Memory Fetcher

--- src/main.rs
+++ src/main.rs
@@ -2,61 +2,72 @@ use std::{
         process::{Command}, 
         io::prelude::Read,
         fs::File, ffi::OsString,
-        time::Duration
-};
-
+        time::Duration,
+    };
+    
 fn main() -> std::io::Result<()> {
 let hostname = File::open("/etc/hostname").unwrap();
 
 let info = Command::new("head")
-        .arg("-1")
-        .stdin(hostname)
-        .output()
-        .expect("Error doing whatever"); 
+    .arg("-1")
+    .stdin(hostname)
+    .output()
+    .expect("Error doing whatever"); 
 
 let kernel = Command::new("uname")
-        .arg("-r")
-        .output()
-        .expect("Error doing whatever"); 
+    .arg("-r")
+    .output()
+    .expect("Error doing whatever"); 
 let os = Command::new("head")
-        .args(["-1", "/etc/os-release"])
-        .output()
-        .expect("Error doing whatever"); 
+    .args(["-1", "/etc/os-release"])
+    .output()
+    .expect("Error doing whatever"); 
 
 let uptime = fetch_uptime()?;
 
-let memory = Command::new("head")
-        .args(["-1", "/proc/meminfo"])
-        .output()
-        .expect("Error doing whatever"); 
-let total_memory = format!("{}", String::from_utf8_lossy(&memory.stdout).replace("MemTotal:      ", ""));
-print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-
 println!("       {}@{} ===========================",  os_string("USER"), String::from_utf8_lossy(&info.stdout));
-println!("OS: {}Kernel: {}Uptime: {} days, {} hours, {} minutes \nEnvironment: {} \nShell: {} \nMemory: {} ===========================",
+println!("OS: {}Kernel: {}Uptime: {} days, {} hours \nEnvironment: {} \nShell: {}",
 String::from_utf8_lossy(&os.stdout).replace("NAME=", ""), 
 String::from_utf8_lossy(&kernel.stdout), 
-uptime.0,
-uptime.1,
-uptime.2,
+uptime.0, uptime.1,
 os_string("DESKTOP_SESSION"), 
-os_string("SHELL"),
-total_memory
+os_string("SHELL")
 );
+fetch_mem();
+
 Ok(())
 }
 
 fn os_string(string: &str) -> String {
-        std::env::var_os(string)
-            .unwrap_or(OsString::from("unable to determine."))
-            .into_string()
-            .unwrap()
+    std::env::var_os(string)
+        .unwrap_or(OsString::from("unable to determine."))
+        .into_string()
+        .unwrap()
 }
 
-fn fetch_uptime() -> std::io::Result<(u64, u64, u64)> {
+fn fetch_mem() -> std::io::Result<(String, String)> {
+    let text = std::fs::read_to_string("/proc/meminfo").unwrap();
+    let a = "0"; // placeholder variables
+    let b = "0";
+    for line in text.lines() {
+        if line.contains("MemTotal:") {
+            let tpretty = line.replace("MemTotal:", "").replace("kB", "").replace(" ", ""); 
+             let total: u32 = tpretty.parse().expect("Oh No");
+            let mb = total / 1000;
+            println!("Memory: {}Mb", mb);
+        };/* Commented Out For Now
+        if line.contains("MemAvailable:") {
+           let apretty = line.replace("MemAvailable:", "").replace("kB", "").replace(" ", "");
+            println!("Memory Available: {}", apretty);
+        }; */
+}
+Ok((a.to_string(), b.to_string()))
+}
+
+fn fetch_uptime() -> std::io::Result<(u64, u64)> {
     let mut file = File::open("/proc/uptime")?;
     let mut c = String::new();
-    file.read_to_string(&mut c)?;
+     file.read_to_string(&mut c)?;
     let uptime = Duration::from_secs_f32(
         c.split_whitespace()
             .next()
@@ -66,6 +77,7 @@ fn fetch_uptime() -> std::io::Result<(u64, u64, u64)> {
     );
     let d = uptime.as_secs() / 86400;
     let h = (uptime.as_secs() - d * 86400) / 3600;
-    let m = (uptime.as_secs() - h * 12240) / 60;
-    Ok((d, h, m))
+    /* Commented out because of overflow issues past 2 days and 6 hours of uptime with this piece will fix later
+      let m = (uptime.as_secs() - h * 12240) / 60;*/
+    Ok((d, h))
 }

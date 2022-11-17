6f18f58056f1936cfd108c5b1156fabffe90769c
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-11-17 16:01:04 -0600

    Minor Performance Improvements
    Using Chars instead of strings in certain places & Replaced deprecated unwrap_or with unwrap_or_else.

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ let distro = fetch_distro()?;
 let def = "ascii/{}";
  let path = def.replace("{}", &distro.to_lowercase());
   let distro_ascii = std::fs::read_to_string(path)?;
-  let line_one = distro_ascii.lines().find(|f| f.contains("1")).unwrap();
-  let line_two = distro_ascii.lines().find(|f| f.contains("2")).unwrap();
-  let line_three = distro_ascii.lines().find(|f| f.contains("3")).unwrap();
-  let line_four = distro_ascii.lines().find(|f| f.contains("4")).unwrap();
-  let line_five = distro_ascii.lines().find(|f| f.contains("5")).unwrap();
-  let line_six = distro_ascii.lines().find(|f| f.contains("6")).unwrap();
-  let line_seven = distro_ascii.lines().find(|f| f.contains("7")).unwrap();
-  let line_eight = distro_ascii.lines().find(|f| f.contains("8")).unwrap();
+  let line_one = distro_ascii.lines().find(|f| f.contains('1')).unwrap();
+  let line_two = distro_ascii.lines().find(|f| f.contains('2')).unwrap();
+  let line_three = distro_ascii.lines().find(|f| f.contains('3')).unwrap();
+  let line_four = distro_ascii.lines().find(|f| f.contains('4')).unwrap();
+  let line_five = distro_ascii.lines().find(|f| f.contains('5')).unwrap();
+  let line_six = distro_ascii.lines().find(|f| f.contains('6')).unwrap();
+  let line_seven = distro_ascii.lines().find(|f| f.contains('7')).unwrap();
+  let line_eight = distro_ascii.lines().find(|f| f.contains('8')).unwrap();
   
 print!("       {}@{} =========================== 
 {} OS: {} 
@@ -50,21 +50,20 @@ line_eight);
 
 fn title() -> std::io::Result<(String, String)> {
    let host = std::fs::read_to_string("/etc/hostname").unwrap();
-    let hostname = format!("{}", host);
-   let username = std::env::var_os("USER").unwrap_or("string".into()).into_string().unwrap();
-   Ok((username, hostname))
+   let username = std::env::var_os("USER").unwrap_or_else(|| "string".into()).into_string().unwrap();
+   Ok((username, host.to_string()))
 }
 
 fn fetch_desktop() -> std::io::Result<(String, String)> {
-   let desktop = std::env::var_os("DESKTOP_SESSION").unwrap_or("string".into()).into_string().unwrap();
-   let shell = std::env::var_os("SHELL").unwrap_or("string".into()).into_string().unwrap();
+   let desktop = std::env::var_os("DESKTOP_SESSION").unwrap_or_else(|| "string".into()).into_string().unwrap();
+   let shell = std::env::var_os("SHELL").unwrap_or_else(|| "string".into()).into_string().unwrap();
    Ok((desktop, shell))
 }
 
 fn fetch_cpu() -> std::io::Result<String> {
   let reader = std::fs::read_to_string("/proc/cpuinfo")?;
    let line = reader.lines().find(|f| f.contains("model name")).unwrap();
-  let output = line.replace("model name", "").replace("CPU", " - ").replace("\t", "").replace(": ", "");
+  let output = line.replace("model name", "").replace("CPU", " - ").replace('\t', "").replace(": ", "");
 
    Ok(output)
 }
@@ -80,9 +79,9 @@ fn fetch_distro() -> std::io::Result<String> {
 fn fetch_mem() -> std::io::Result<(u32, u32)> {
    let meminfo = std::fs::read_to_string("/proc/meminfo").unwrap();
     let line_one = meminfo.lines().find(|f| f.contains("MemAvailable:")).unwrap();
-     let pretty_one = line_one.replace("MemAvailable:", "").replace(" ", "").replace("kB", "");
+     let pretty_one = line_one.replace("MemAvailable:", "").replace(' ', "").replace("kB", "");
     let line_two = meminfo.lines().find(|f| f.contains("MemTotal:")).unwrap();
-     let pretty_two = line_two.replace("MemTotal:", "").replace(" ", "").replace("kB", "");
+     let pretty_two = line_two.replace("MemTotal:", "").replace(' ', "").replace("kB", "");
     let total_one: u32 = pretty_one.parse().expect("Error at 52:23");
      let mem_temp = total_one / 1000;
     let total_two: u32 = pretty_two.parse().expect("Error at 54:23");
